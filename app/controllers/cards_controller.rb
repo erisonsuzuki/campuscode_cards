fee68aafff467fd1b441ab81fcb7e9c824929c03
melost <lizzy.maddog@gmail>
2016-10-26 21:54:10 -0200

    Feature: creates form for card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,5 @@
 class CardsController < ApplicationController
+  before_filter :target_card, only: [:edit, :update]
 	def index
 		@cards = Card.all
 	end
@@ -8,11 +9,31 @@ class CardsController < ApplicationController
   end
 
   def new
+    @card = Card.new
   end
 
   def create
-    Card.create(frente: params[:frente], verso: params[:verso])
+    card = Card.create(card_params)
 
-    redirect_to root_path
+    redirect_to card_url(card)
+  end
+
+  def edit
+  end
+
+  def update
+    @card.update(card_params)
+
+    redirect_to card_url(@card)
+  end
+
+  private
+
+  def target_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:frente, :verso)
   end
 end
